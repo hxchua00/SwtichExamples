a1a7f45153613b55ef4a8ac48abae37c45be0a56
hxchua00 <hxchua00@gmail.com>
2021-09-18 21:11:31 +0800

    Add project files.

---
+++ SwtichExamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwtichExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose from the options: ");
+            Console.WriteLine("Option 1");
+            Console.WriteLine("Option 2");
+            Console.WriteLine("Option 3");
+
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("I'm at Option 1");
+                    break;
+                case 2:
+                    Console.WriteLine("I'm at Option 2");
+                    break;
+                case 3:
+                    Console.WriteLine("I'm at Option 3");
+                    break;
+                default:
+                    Console.WriteLine("Please choose from the option available!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
